133251cfb54f7ea80e056ebb3fecc53aaf62dcb2
Inaki <inakipoch106@gmail.com>
2023-04-13 17:28:00 -0300

    struct Tarea added

---
+++ tp4_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Tarea {
+    int assignmentID;
+    char* description;
+    int duration;
+} Assignment;
+
+int main(int argc,char** argv) {
+    return 0;
+}
