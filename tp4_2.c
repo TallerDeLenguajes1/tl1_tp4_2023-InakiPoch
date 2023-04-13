2b3fd4a2988e5399cccab3c3681c134b903146b0
Inaki <inakipoch106@gmail.com>
2023-04-13 18:10:15 -0300

    Added interface to initialize assigments

--- tp4_2.c
+++ tp4_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Tarea {
     int assignmentID;
@@ -7,6 +8,30 @@ typedef struct Tarea {
     int duration;
 } Assignment;
 
+void assignmentsInterface(Assignment** assignment,int number) {
+    char* buffer = malloc(100*sizeof(*buffer));
+    for(int i = 0 ; i < number ; i++) {
+        assignment[i] = malloc(sizeof(*(assignment[i])));
+        printf("\n------TAREA %d------\n",i+1);
+        assignment[i]->assignmentID = i + 1;
+        printf("Descripcion de la tarea: ");
+        gets(buffer);
+        assignment[i]->description = malloc(strlen(buffer) + 1);
+        strcpy(assignment[i]->description,buffer);
+        printf("\nDuracion de la tarea: ");
+        scanf("%d",&assignment[i]->duration);
+    }
+    free(buffer);
+}
+
 int main(int argc,char** argv) {
+    int numberAssignments;
+    printf("Ingresar la cantidad de tareas a realizar: ");
+    scanf("%d",&numberAssignments);
+    Assignment** assignments = malloc(numberAssignments*sizeof(*assignments));
+    for(int i = 0 ; i < numberAssignments ; i++) {
+        assignments[i] = NULL;
+    }
+
     return 0;
 }
